0407ff07f8f84ca31c78e29a2dbcd6e83169e799
Natalie Burgei <natalieburgei04@gmail.com>
2024-01-27 00:53:01 +0300

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_list = []
+        loads_list =json.loads(self.get_response_body())
+        for item in loads_list:
+            response_list.append(item)
+
+        return response_list
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
